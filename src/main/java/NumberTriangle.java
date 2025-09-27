4cc9d53dc51699d2d6f61796effbe13d16d42397
Nameer Qadir <nameer.qadir@mail.utoronto.ca>
2025-09-27 13:17:50 -0400

    fix

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -110,11 +110,6 @@ public class NumberTriangle {
 
 }
 
-
-        return -1;
-    }
-
-
     /** Read in the NumberTriangle structure from a file.
      *
      * You may assume that it is a valid format with a height of at least 1,
